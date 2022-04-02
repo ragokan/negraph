3c0200b61b03a78980fe08e14578802355340102
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-04-02 21:20:52 +0300

    za

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,11 @@ import { AppService } from "./app.service";
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * Get hello message!
+   *
+   * @returns {string}
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
